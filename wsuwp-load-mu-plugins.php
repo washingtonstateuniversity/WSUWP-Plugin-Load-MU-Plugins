7509b348b8c57a9cb1c3a74249b76f84a3236bbd
Jeremy Felt <jeremy.felt@gmail.com>
2017-12-06 10:04:54 -0800

    Bump version 0.0.4

--- wsuwp-load-mu-plugins.php
+++ wsuwp-load-mu-plugins.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSUWP Load MU Plugins
-Version: 0.0.3
+Version: 0.0.4
 Description: Loads a list of mu-plugins.
 Author: washingtonstateuniversity, jeremyfelt
 Author URI: https://web.wsu.edu/
