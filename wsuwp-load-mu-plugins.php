3795c601a537f9022e66d972b44c39662bc7be97
Jeremy Felt <jeremy.felt@gmail.com>
2018-03-13 13:57:37 -0700

    Add the wsuwp-mu-cmb2.php plugin to loader

--- wsuwp-load-mu-plugins.php
+++ wsuwp-load-mu-plugins.php
@@ -19,6 +19,7 @@ add_filter( 'wsuwp_load_mu_plugins', 'wsuwp_add_mu_plugins' );
 function wsuwp_add_mu_plugins() {
 	return array(
 		'cavalcade/plugin.php',
+		'wsuwp-mu-cmb2/wsuwp-mu-cmb2.php',
 		'wsuwp-mu-extended-batcache/wsuwp-mu-extended-batcache.php',
 		'wsuwp-mu-simple-filters/wsuwp-mu-simple-filters.php',
 		'wsuwp-mu-new-site-defaults/plugin.php',
